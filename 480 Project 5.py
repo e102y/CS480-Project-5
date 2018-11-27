0ffa3fdd95154011f0458a9870c8f6d49ecb674f
e102y <e102y@users.noreply.github.com>
2018-11-27 11:31:39 -0800

    added definition for part 1 and part 2 perceptrons

--- 480 Project 5.py
+++ 480 Project 5.py
@@ -1,12 +1,18 @@
+import random as r
+#import numpy as np #for argmax in multiclass perceptron
 def features(x, N, M): #input NxM image(2d aray [N][M]) x, output length 6 feature array
     # 1)
     Density = sum(x)/(N*M)
     # 2)
-    DOFS += x[i][j] ^ x[j][i] for i in range(N) for j in range(M)
+    for i in range(N):
+        for j in range(M):
+            DOFS += x[i][j] ^ x[j][i]
     DOFS /= (N*M)
     # 3,4,5,6)
     BW = [[0]*M]*N #initialize a new NxM array
-    BW[i][j] = int(x[i][j]<=128) for i in range(N) for j in range(M) #thresholding operation
+    for i in range(N):
+        for j in range(M):
+            BW[i][j] = int(x[i][j]<=128)#thresholding operation
     # 3,4)
     Cols = []
     # 5,6)
@@ -40,5 +46,47 @@ def features(x, N, M): #input NxM image(2d aray [N][M]) x, output length 6 featu
     return [Density, DOFS, maxInterHoriz, aveInterHoriz, maxInterVert, aveInterVert]
     #order: [Density, Degree/Measure of Symettry, maximum horizontal intersections, average horizntal intersections, maximum vertical intersections, average vertical intersections]
 
-    
-    
+class Perceptron:
+    def __init__(self):
+        self.W = [(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10)]
+    def pTrain(self, inp, L, O, n ): #where L is [0,1]
+        for a in range(len(self.W)):
+            self.W[a] = self.W[a] + n*(O-L)*inp[a]
+    def pProcess(self, x):
+        temp = 0
+        for a in range(len(self.W)):
+            temp += x[a]*self.W[a]
+        return int(temp > 0)
+    def getWeights(self):
+        return self.W
+
+class MultiPerceptron:
+    def __init__(self):
+        self.W = [[(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10)],#0
+                  [(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10)],#1
+                  [(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10)],#2
+                  [(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10)],#3
+                  [(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10)],#4
+                  [(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10)],#5
+                  [(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10)],#6
+                  [(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10)],#7
+                  [(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10)],#8
+                  [(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10),(((r.random()*2)-1)/10)]]#9
+    def pTrain(self, inp, L, O, n ): #where L is [0,9]
+        if(O == L):
+            for a in range(len(self.W[L])):
+                self.W[L][a] = self.W[L][a] + n*inp[a]
+        else:
+            for a in range(len(self.W[L])):
+                self.W[L][a] = self.W[L][a] - n*inp[a]
+    def pProcess(self, x):
+        tomp = []
+        for a in range(len(self.W)):
+            temp = 0
+            for b in range(len(self.W[a])):
+                temp += x[b]*self.W[a][b]
+            tomp.append(temp)
+        return tomp.index(max(tomp))
+    def getWeights(self):
+        return self.W
+class NNwrk:
